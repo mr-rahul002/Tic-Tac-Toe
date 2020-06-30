847fd11e15d5de8c518f9e021c2f3f0e6f267cd6
Rahul Seth <rahulseth@localhost.localdomain>
2020-06-30 09:55:29 +0530

    bot

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:Tic_Tac_toe/StartPage/StartPage.dart';
-
 import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
 
 void main() => runApp(TicTacToe());
 
-class TicTacToe extends StatelessWidget {
+class TicTacToe extends StatefulWidget {
+  @override
+  _TicTacToeState createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: StartPage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: StartPage());
   }
 }
