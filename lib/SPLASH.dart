fc57810cc4b33c70e9ed72069463cd1cdcdabf13
Rahul Seth <rahulseth@localhost.localdomain>
2020-08-08 08:40:46 +0530

    auth

--- lib/SPLASH.dart
+++ lib/SPLASH.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
+
+import 'package:Tic_Tac_toe/LoginScreen/SignIn.dart';
 import 'package:Tic_Tac_toe/Players/Players.dart';
-import 'package:Tic_Tac_toe/StartPage/StartPage.dart';
-import 'package:Tic_Tac_toe/storage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -11,37 +12,62 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  SharedPreferences logindata;
+  bool newUser;
   @override
   void initState() {
-    data();
     super.initState();
+    startTimer();
   }
 
-  Future<void> data() async {
-    final _auth = FirebaseAuth.instance;
-    final user = await _auth.currentUser();
-    var data = context.watch<Storage>();
-    if (user == null) {
-      await data.storeData();
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => StartPage()));
-    } else {
-      await data.readData();
+  Future<void> checkUser() async {
+    logindata = await SharedPreferences.getInstance();
+    newUser = (logindata.getBool('login') ?? true);
+    print(newUser);
+    if (newUser == false) {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => Players()));
     }
   }
 
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Storage(),
-      child: Scaffold(
-          body: Center(
-              child: Container(
-                  height: 200,
-                  width: 200,
-                  child: Image.asset("assets/onchat.png")))),
-    );
+    return Scaffold(
+        body: Center(
+            child: Container(
+                height: 200,
+                width: 200,
+                child: Image.asset("assets/TicTacToe.png"))));
+  }
+
+  void startTimer() {
+    Timer(Duration(seconds: 3), () {
+      navigateUser();
+    });
+  }
+
+  Future<void> navigateUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var status = prefs.getBool('isLoggedIn') ?? false;
+
+    print(status);
+    if (status) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Players(),
+          ));
+    } else {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SignIn(),
+          ));
+    }
   }
 }
